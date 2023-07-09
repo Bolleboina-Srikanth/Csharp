82956fa7b0cf0a58f7efcde3f16864450898efd6
Bolleboina-Srikanth <srikanthbolleboina456@gmail.com>
2023-07-09 18:07:33 +0530

    [srikanth] add:Exceptionhandlingfiles

---
+++ MinimumAccountBalance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCEPTION_HANDLING
+{
+    public class MinimumAccountBalance : Exception
+    {
+        String message;
+        public MinimumAccountBalance(String message)
+        {
+            this.message = message;
+        }
+        public override string HelpLink 
+        {
+        get
+            {
+                return this.message;
+            }
+        }
+    }
+    public class UserDefinedException
+    {
+        static double current_balance = 1000;
+        public static void Main()
+        {
+            Console.WriteLine("enter amount to withdraw");
+            int amount = int.Parse(Console.ReadLine());
+            try
+            {
+                if(current_balance<amount)
+                {
+                    throw new MinimumAccountBalance("Insufficient funds! your current balance is " + current_balance);
+                }
+                else if(amount<0)
+                {
+                    throw new MinimumAccountBalance("please enter a valid amount");
+                }
+                else
+                {
+                    Console.WriteLine("please take the money " + amount);
+                    Console.ReadLine();
+                }
+            }
+            catch(MinimumAccountBalance e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.HelpLink);
+               // Console.WriteLine(e.Source);
+               // Console.WriteLine(e.StackTrace);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCEPTION_HANDLING
+{
+    internal class Program
+    {
+        public static void DivideByZeroException()
+        {
+            try
+            {
+                Console.WriteLine("Enter any number");
+                int dividend = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter a divisor to divide");
+                int divisor = int.Parse(Console.ReadLine());
+                int result = dividend / divisor;
+                Console.WriteLine(result);
+                Console.ReadLine();
+                
+
+            }
+            catch (ArithmeticException ex)
+            {
+               // Console.WriteLine("catch block executed");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+              //  Console.WriteLine("catch block executed");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Write("finally block is used to close connections");
+                Console.ReadLine();
+
+            }
+        }
+         static  void IndexOutOfBoundEx()
+         {
+            try
+            {
+             int[] arr = new int[5];
+             Console.WriteLine("enter 5 inter numbers");
+             for (int i = 0; i <= arr.Length; i++)
+             {
+                 arr[i] = int.Parse(Console.ReadLine());
+             }
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(MinimumAccountBalance ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+         }
+
+       /* public static void Main(string[] args)
+           {
+            DivideByZeroException();
+            IndexOutOfBoundEx();
+
+           }*/
+    }
+}
